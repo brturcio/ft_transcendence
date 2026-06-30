a17d2ecae0d2092b91c3880610a74827639eb872
Bryan <btjosueturcios@gmail.com>
2026-06-30 14:31:19 +0200

    push htts test

--- front/src/utils/i18n.ts
+++ front/src/utils/i18n.ts
@@ -1,37 +1,36 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import enLang from "./i18n/locales/en/en"
-import esLang from "./i18n/locales/es/es"
-import frLang from "./i18n/locales/fr/fr"
+import enLang from "./i18n/locales/en/en";
+import esLang from "./i18n/locales/es/es";
+import frLang from "./i18n/locales/fr/fr";
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-  en: {
-    translation: enLang,
-  },
-  es: {
-    translation: esLang,
-  },
-  fr: {
-    translation: frLang,
-  }
+	en: {
+		translation: enLang,
+	},
+	es: {
+		translation: esLang,
+	},
+	fr: {
+		translation: frLang,
+	},
 };
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-	fallbackLng: "en",
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+	.init({
+		resources,
+		fallbackLng: "en",
+		lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+		// you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+		// if you're using a language detector, do not define the lng option
 
-    interpolation: {
-      escapeValue: false // react already safes from xss
-    }
-  });
+		interpolation: {
+			escapeValue: false, // react already safes from xss
+		},
+	});
 
-  export default i18n;
+export default i18n;
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from "fs"
 
-// https://vite.dev/config/
-	export default defineConfig({
-  	plugins: [
+export default defineConfig({
+	plugins: [
 		react(),
 		tailwindcss(),
 	],
+	server: {
+		host: "0.0.0.0",
+		port: 3000,
+		https: {
+			cert: fs.readFileSync("10.13.247.134.pem"),
+			key: fs.readFileSync("10.13.247.134-key.pem")
+		},
+	},
 })
+
